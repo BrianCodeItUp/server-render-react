5851bec611d093f380ef9ebcd406bc896be1d5a6
brian <wl00387719@gmail.com>
2021-03-09 23:47:57 +0800

    Add client webpack config and express serving static files

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm the home component</div>
+  return (
+    <div>
+      I'm the home component !!
+      <button onClick={() => console.log('hello')}>Click me</button>
+    </div>
+    )
 }
 
 export default Home;
+ 
