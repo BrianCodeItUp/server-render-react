e99d792f5a883e40a3d051039001218c15efc8e7
brian <wl00387719@gmail.com>
2021-04-08 23:55:09 +0800

    feat: setup client request proxy

--- src/client/client.js
+++ src/client/client.js
@@ -7,11 +7,16 @@ import { Provider } from 'react-redux'
 import React from 'react';
 import ReactDom from 'react-dom';
 import Routes from './Routes'
+import axios from 'axios';
 import reducers from './reducers'
 import { renderRoutes } from 'react-router-config';
 import thunk from 'redux-thunk';
 
-const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk));
+const axiosInstance = axios.create({
+  baseURL: '/api',
+});
+
+const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk.withExtraArgument(axiosInstance)));
 
 // Start up point for the client side application
 ReactDom.hydrate(
